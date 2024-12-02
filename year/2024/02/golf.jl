2ea16d7a891570ec5d3bf9996502c323a87b8c31
alemelis <alessandro_melis@rocketmail.com>
2024-12-02 16:58:58 +0000

    shorter but not shortest

--- year/2024/02/golf.jl
+++ year/2024/02/golf.jl
@@ -1,5 +1,4 @@
 L=readlines(stdin).|>l->parse.(Int,split(l))
 ~,!,s=sum,length,issorted
-C(l)=(s(l)||s(l,rev=true))&&all(0 .<extrema(abs,diff(l)).<4)
-K(l)=C(l)||any(1:!l.|>i->C([l[j] for j=1:!l if j!=i]))
-println.([C~L,K~L])
+C(l)=(s(l)||s(l,rev=1<2))*abs.(diff(l))⊆1:3
+print(C~L," ",(l->C(l)||any(1:!l.|>i->C([l[j] for j=1:!l if j!=i])))~L)
