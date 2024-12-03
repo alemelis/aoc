d092e20b708498422955ee50a9949d4328312c66
alemelis <alessandro_melis@rocketmail.com>
2024-12-03 11:14:46 +0000

    day3

--- year/2024/02/solve.py
+++ year/2024/02/solve.py
@@ -1,12 +1,8 @@
 with open("input.txt", 'r') as f:
-    lines = f.readlines()
-
-levels = []
-for line in lines: # O(n)
-    levels.append([*map(int, line.strip().split())])
+    levels = [[*map(int, line.strip().split())] for line in f.readlines()] # O(n)
 
 def check(line):
-    if sorted(line) != line and sorted(line, reverse=True) != line: # O(len(line) log(len(line)))
+    if sorted(line) != line and sorted(line, reverse=True) != line: # O(len(line) log(len(line))) = const
         return False
     for i in range(len(line)-1):
         if not 0<abs(line[i]-line[i+1])<4:
@@ -14,9 +10,7 @@ def check(line):
     return True
 
 # *
-count = 0
-for line in levels: # O(n * m log m); n = num lines, m = len(line)
-    count += check(line)
+count = sum(check(line) for line in levels) # O(n)
 print(count)
 
 # **
@@ -24,13 +18,11 @@ def check2(line):
     if check(line):
         return True
 
-    for i in range(len(line)): # O(len(line)^2 log(len(line)))
+    for i in range(len(line)): # O(len(line)^2 log(len(line))) = const
         new_line = [e for j, e in enumerate(line) if j!=i]
         if check(new_line):
             return True
     return False
 
-count = 0
-for line in levels: # O(n * m^2 log m)
-    count += check2(line)
+count = sum(check2(line) for line in levels) # O(n)
 print(count)
---
+++ year/2024/03/solve.jl
@@ -0,0 +1,19 @@
+input = join(readlines("input.txt"))
+!m=parse(Int,m)
+
+# *
+sum(eachmatch(r"mul\(([0-9]+),([0-9]+)\)",input).|>m->!m[1]*!m[2])|>println
+
+# **
+~=isnothing
+function solve(i,s=0,mul=1)
+  for m=eachmatch(r"(do\(\))|(don\'t\(\))|mul\(([0-9]+),([0-9]+)\)",i)
+    if ~m[1]&&~m[2]
+      s += mul*(!m[3]*!m[4])
+    else
+      mul = ~m[1] ? 0 : 1
+    end
+  end
+  println(s)
+end
+solve(input)
