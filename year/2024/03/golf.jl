e636f39dc4fa1f5b58f1a88d869a5da6c536632a
alemelis <alessandro_melis@rocketmail.com>
2024-12-04 11:00:54 +0000

    day4

--- year/2024/03/golf.jl
+++ year/2024/03/golf.jl
@@ -1,5 +1,5 @@
-L=join(readlines("input.txt"))
+L=join(eachline())
 !m=parse(Int,m)
 e,~=eachmatch,isnothing
-S(i,s=0,M=1)=(e(r"(do\(\))|(don\'t\(\))|mul\(([0-9]+),([0-9]+)\)",i).|>m->~m[1]&&~m[2] ? (s+=(!m[3]*!m[4])M) : (M=[1,0][1+~m[1]]);s)
-print(sum(e(r"mul\(([0-9]+),([0-9]+)\)",L).|>m->!m[1]*!m[2])," ",S(L))
+S(i,s=0,M=1)=(e(r"(do\(\))|(don\'t\(\))|mul\((\d+),(\d+)\)",i).|>m->~m[1]&&~m[2] ? (s+=(!m[3]*!m[4])M) : (M=1-~m[1]);s)
+print(sum(e(r"mul\((\d+),(\d+)\)",L).|>m->!m[1]*!m[2])," ",S(L))
---
+++ year/2024/04/solve.jl
@@ -0,0 +1,23 @@
+using LinearAlgebra
+
+rows = readlines("input.txt")
+line = join(rows)
+
+# *
+l = length(rows[1])
+rs = [r"XMAS", r"SAMX"]
+tot = sum(s->sum(r->count(s, r), rows),rs) # horizontal
+tot += sum(s->sum(i->count(s, line[i:l:end]), 1:l),rs) # vertical
+
+# diagonals
+M = permutedims(reshape([line...], l, l))
+tot += sum(A->sum(s->sum(i->count(s, join(diag(A, i))), -l:l), rs), [M, reverse(M, dims=2)])
+
+println(tot)
+
+# **
+sum(r->sum(k->sum(j->count(r, join(line[i*l+k:i*l+k+2] for i=j:j+2)),
+	0:l-3), # j
+	1:l-2), # k
+	[r"M.S.A.M.S", r"M.M.A.S.S", r"S.S.A.M.M", r"S.M.A.S.M"] # r
+	)|>println
