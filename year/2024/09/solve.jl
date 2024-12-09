34a84b09cdc037db1301f17cf1325ffdb79e620b
alemelis <alessandro_melis@rocketmail.com>
2024-12-09 15:37:09 +0000

    tidier?

--- year/2024/09/solve.jl
+++ year/2024/09/solve.jl
@@ -1,16 +1,24 @@
 line = readchomp("input.txt")
 nums = [line...]
-function solve1(nums)
+
+function map_disk(nums)
     disk_map = []
     id = 0
     for (i,n)=enumerate(nums)
         if i%2>0
-            push!(disk_map, repeat([id],parse.(Int, n))...)
+            push!(disk_map, repeat([id],parse.(Int, n)))
             id += 1
         else
-            push!(disk_map, repeat([-1], parse.(Int, n))...)
+            push!(disk_map, repeat([-1], parse.(Int, n)))
         end
     end
+    disk_map
+end
+
+disk_map = map_disk(nums)
+
+function solve1(disk_map)
+    disk_map=vcat(disk_map...)
 
     nempty=count(==(-1),disk_map)
     disk_map[disk_map.==-1] .= reverse(disk_map[disk_map.!=-1])[1:nempty]
@@ -18,77 +26,43 @@ function solve1(nums)
 
     sum([0:length(disk_map)-1;].*disk_map)|>println
 end
-solve1(nums)
+solve1(disk_map)
 
-function solve2(nums)
-    disk_map = []
-    id = 0
-    for (i,n)=enumerate(nums)
-        if i%2>0
-            push!(disk_map, repeat([id], parse.(Int, n)))
-            id += 1
-        else
-            push!(disk_map, repeat([-1], parse.(Int, n)))
-        end
-    end
-
-    files = []
-    for (i,f)=enumerate(disk_map)
-        if all(f.>-1)
-            push!(files, [i,f])
-        end
-    end
-
-    free = []
-    for (i,e) = enumerate(disk_map)
-        if all(e.<0)
-            push!(free, (i,e))
-        end
-    end
+function solve2(disk_map)
+    files = [(i,f) for (i,f)=enumerate(disk_map) if all(f.>1)]
+    free = [(i,e) for (i,e)=enumerate(disk_map) if all(e.<0)]
 
     j = length(files)
-    used = 0
     while j>1
         f = files[j]
-        j-=1
+        j -= 1
         file_len = length(f[2])
-        if file_len == 0
-            continue
-        end
+        file_len == 0 && continue
 
         for (i,e)=free
             f1 = f[1]
-            if length(e) >= file_len && i<f1
-                new_free = repeat([-1], file_len)
-                disk_map[f1] = new_free
+            free_len = length(e)
+            if free_len >= file_len && i<f1
+                disk_map[f1] = repeat([-1], file_len)
                 disk_map[i] = f[2]
 
-                if file_len<length(e)
-                    insert!(disk_map, i+1, repeat([-1], length(e)-file_len))
-                    files = [(fi>i ? fi+1 : fi,fe) for (fi,fe)=files]
+                if file_len<free_len
+                    insert!(disk_map, i+1, repeat([-1], free_len-file_len))
+                    files = [(fi>i ? fi+1 : fi, fe) for (fi, fe)=files]
                 end
 
-                free = []
-                for (i,e) = enumerate(disk_map)
-                    if all(e.<0)
-                        push!(free, (i,e))
-                    end
-                end
+                free = [(i,e) for (i,e)=enumerate(disk_map) if all(e.<0)]
                 break
             end
         end
     end
 
-    disk_map = vcat(disk_map...)
-    s= 0
-    i=-1
-    for d=disk_map
+    s,i = 0, -1
+    for d=vcat(disk_map...)
         i+=1
-        if d==-1
-            continue
-        end
+        d==-1 && continue
         s+=i*d
     end
-    println(s)
+    s |> println
 end
-solve2(nums)
+solve2(disk_map)
