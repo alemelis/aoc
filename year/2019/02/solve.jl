9100db28859b566ee81d598bb11d41a94429cfbf
alemelis <alessandro_melis@rocketmail.com>
2025-01-10 13:22:15 +0000

    2019-02

--- year/2019/02/solve.jl
+++ year/2019/02/solve.jl
@@ -1,14 +1,9 @@
 line = collect(eval(Meta.parse(readchomp("input.txt"))))
 
-#=
-before running the program
-replace position 1 with the value 12
-and replace position 2 with the value 2
-=#
-line[2] = 12
-line[3] = 2
+function solve(line, noun, verb)
+    line[2] = noun
+    line[3] = verb
 
-function solve(line)
     i = 1
     while line[i] != 99
         op = line[i]
@@ -19,6 +14,41 @@ function solve(line)
         line[dst+1] = eval(Meta.parse("$a $(op == 1 ? "+" : "*") $b"))
         i += 4
     end
-    println(line[1])
+    line[1]
 end
-solve(line)
+
+# *
+solve(deepcopy(line), 12, 2) |> println
+
+# **
+function binsearch(line)
+    target = 19690720
+    a, b = 0, 99
+    noun = 0
+    while a<b
+        noun = (a+b)÷2
+        r = solve(deepcopy(line), noun, 0)
+        if 0 < r + 99 - target < 99
+            break
+        elseif r < target
+            a = noun+1
+        elseif r > target + 99
+            b = noun-1
+        end
+    end
+    a, b = 0, 100
+    verb = 0
+    while a<b
+        verb = (a+b)÷2
+        r = solve(deepcopy(line), noun, verb)
+        if r < target
+            a = verb+1
+        elseif r > target
+            b = verb-1
+        else
+            break
+        end
+    end
+    100 * noun + verb
+end
+binsearch(line) |> println
